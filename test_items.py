38285fcc32630a8b97eee6d014a032fc3df70b76
rrudiuk <rus.barik@gmail.com>
2020-07-18 23:09:59 +0300

    Renamed test function

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_check_button_presence(browser):
+def test_check_add_to_card_button(browser):
     browser.get(link)
     add_to_card_btn = browser.find_element_by_css_selector("button.btn-add-to-basket")
     time.sleep(5)
